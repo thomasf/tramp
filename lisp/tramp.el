591897479519e304574cbe9f9de0891ffd20433c
grossjoh <grossjoh>
1999-02-22 16:46:44 +0000

    rssh-handle-write-region: backward compatibility.         Kudos to Edward J. Sabol <sabol@alderaan.gsfc.nasa.gov>.

--- lisp/tramp.el
+++ lisp/tramp.el
@@ -708,7 +708,9 @@ Bug: output of COMMAND must end with a newline."
     (setq tmpfil (make-temp-name "/tmp/rssh."))
     (rssh-run-real-handler
      'write-region
-     (list start end tmpfil append 'no-message lockname confirm))
+     (if confirm ; don't pass this arg unless defined for backward compat.
+         (list start end tmpfil append 'no-message lockname confirm)
+       (list start end tmpfil append 'no-message lockname)))
     (call-process rssh-scp-program nil nil nil
                   tmpfil
                   (format "%s@%s:%s"
