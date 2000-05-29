10d49f161e0af2943e7d5bd0aefc37d41199ed1d
daniel <daniel>
2000-05-29 12:51:41 +0000

    Send the Perl `file-attributes' implementation on several lines, to avoid issues with brain-dead sh(1) implementations and the like.

--- lisp/tramp.el
+++ lisp/tramp.el
@@ -930,14 +930,14 @@ upon opening the connection.")
 ;; as directories with this code. I still need to dig out why that is.
 ;; --daniel@danann.net
 (defconst rcp-perl-file-attributes (concat
- "$f = $ARGV[0];"
- "@s = lstat($f);"
- "if (($s[2] & 0120000) == 0120000) { $l = readlink($f); $l = \"\\\"$l\\\"\"; }"
- "elsif (($s[2] & 040000) == 040000) { $l = \"t\"; }"
- "else { $l = \"nil\" };"
- "printf(\"(%s %u %u %u (%u %u) (%u %u) (%u %u) %u %u t %u %u)\\n\","
- "$l, $s[3], $s[4], $s[5], $s[8] >> 16, $s[8] & 0xffff, $s[9] >> 16,"
- "$s[9] & 0xffff, $s[10] >> 16, $s[10] & 0xffff, $s[7], $s[2], $s[1], $s[0]);"
+ "$f = $ARGV[0];
+@s = lstat($f);
+if (($s[2] & 0120000) == 0120000) { $l = readlink($f); $l = \"\\\"$l\\\"\"; }
+elsif (($s[2] & 040000) == 040000) { $l = \"t\"; }
+else { $l = \"nil\" };
+printf(\"(%s %u %u %u (%u %u) (%u %u) (%u %u) %u %u t %u %u)\\n\",
+$l, $s[3], $s[4], $s[5], $s[8] >> 16, $s[8] & 0xffff, $s[9] >> 16,
+$s[9] & 0xffff, $s[10] >> 16, $s[10] & 0xffff, $s[7], $s[2], $s[1], $s[0]);"
  )
   "Perl script to produce output suitable for use with `file-attributes'
 on the remote file system.")
